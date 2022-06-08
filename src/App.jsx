084c99cd68e3b0ed89cfc80d5fe8bfbda25895b6
Johannes Jakobsson <jajo21pg@student.ju.se>
2022-06-09 00:47:20 +0200

    Flyttade mappen pages

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<App />}>
+      <Route path='/' element={<App />}> 
         <Route index element={<Home />} />
         <Route path='home' element={<Home />} />
         <Route path='experience' element={<Experience />} />

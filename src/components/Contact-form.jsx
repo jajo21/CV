0900f36c74aad0ba7eb5a92e7e454cc392ecd67a
Johannes Jakobsson <jajo21pg@student.ju.se>
2022-06-12 19:33:05 +0200

    Har lagt till kontaktsidan med react-hook-form och emailJS

---
+++ src/components/Contact-form.jsx
@@ -0,0 +1,96 @@
+import React, {useRef, useState, useEffect} from 'react'
+import { useForm } from 'react-hook-form';
+import emailjs from '@emailjs/browser';
+
+function ContactForm() {
+    const form = useRef();
+    const [sent, setSent] = useState(false);
+
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors }
+    } = useForm({
+        defaultValues: {
+            name: '',
+            email: '',
+            message: ''
+        }
+    });
+
+    useEffect(() => {
+        if(watch('name') !== '' || watch('email') !== '' || watch('message') !== '') {
+            setSent(false);
+        }
+    }, [watch('name'), watch('email'), watch('message')])
+
+    const sendEmail = (data, e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('gmail', 'jjakobsson.se', form.current, 'q1LaHj6VMf8SM_Nct')
+            .then((result) => {
+                console.log(result.text);
+                if(result.text == 'OK') {
+                    setSent(!sent);
+                }
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset();
+    };
+    
+    return (
+        <div>
+            <form
+                name="email-form"
+                ref={form}
+                className='email-form'
+                onSubmit={handleSubmit(sendEmail)}>
+                <label htmlFor="name">Namn</label> <br />
+                <input
+                    {...register("name", {
+                        required: "Fältet är obligatoriskt."
+                    })}
+                    type="text"
+                    name="name"
+                />
+                <p className='error'>{errors.name?.message}</p>
+
+                <label htmlFor="email">E-mail</label> <br />
+                <input
+                    {...register("email", {
+                        required: "Fältet är obligatoriskt.",
+                        pattern: {
+                            value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                            message: 'Vänligen skriv in giltig email-address.'
+                        }
+                    })}
+                    type="text"
+                    name="email"
+                />
+                <p className='error'>{errors.email?.message}</p>
+
+                <label htmlFor="message">Meddelande</label> <br />
+                <textarea
+                    {...register("message", {
+                        required: "Fältet är obligatoriskt."
+                    })}
+                    name="message"
+                />
+                <p className='error'>{errors.message?.message}</p>
+
+                <button className='send-button' type='submit'>Skicka</button>
+
+                {sent && 
+                    <div className='message-sent'>
+                        <p>Tack så mycket för ditt meddelande! Jag svarar inom kort.</p>
+                    </div>
+                }
+                
+            </form>
+        </div>
+    )
+}
+
+export default ContactForm;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import ContactForm from '../components/Contact-form'
+import imageUrl from '../images/images.js'
+
+import './css/contact.css'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className='contact'>
+      <div className='contact-pic-div'>
+        <img className='contact-pic' src={imageUrl.meRauk} alt="Johannes på bild" />
+      </div>
+
+      <div className='contact-form-div'>
+        <h1>Kontakta mig</h1>
+        <ContactForm />
+      </div>
+    </div>
   )
 }
 

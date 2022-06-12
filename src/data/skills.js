9d9ec1a979e66d9c81e2ea5fa39b140c9d3ef6bd
Johannes Jakobsson <jajo21pg@student.ju.se>
2022-06-12 15:22:30 +0200

    Har lagt till lite information på home-sidan och stylat den

---
+++ src/data/skills.js
@@ -0,0 +1,34 @@
+export const skills = [
+    {
+        title: 'Front-end',
+        skills: [
+            {title: 'HTML5'},
+            {title: 'CSS'},
+            {title: 'JavaScript'},
+            {title: 'ReactJS'},
+            {title: 'BootStrap'},
+        ]
+    },{
+        title: 'Back-end',
+        skills: [
+            {title: 'C#'},
+            {title: 'ASP.NET Core'},
+            {title: 'ASP.NET Identity Core'},
+            {title: 'Entity Framework Core'},
+            {title: 'REST'},
+            {title: 'API'},
+            {title: 'SQL'},
+            {title: 'Unit-tests'},
+            {title: 'Jwt Bearer Tokens'},
+            {title: 'Swagger UI'},
+        ]
+    },{
+        title: 'Designmönster och tekniker',
+        skills: [
+            {title: 'GIT & GitHub '},
+            {title: 'ASP.NET MVC'},
+            {title: 'Single Page Applications'},
+            {title: 'NPM'},
+        ]
+    }
+]
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,50 +1,73 @@
-import React from 'react';
+import React, {useState} from 'react';
 import imageUrl from '../images/images';
+import {skills} from '../data/skills.js';
 import './css/home.css';
 
 function Home() {
+
+    const [more, setMore] = useState(false);
+
     return (
         <div className='home-div'>
             <div className='home-pic-div'>
                 <img className='home-pic' src={imageUrl.creperiet} alt="Johannes på bild" />
             </div>
-            <div className='home-text'>
-                <h2>Välkommen!</h2>
-                <p>
-                    Mitt namn är Johannes och jag är en 31 årig ung man som är bosatt i Visby. Under min ungdom har jag spenderat många 
-                    sommarlov och helger genom att prova på olika arbetsplatser för att få en bredare blick av arbetslivet. 
-                    Under perioder har jag också varit bosatt och arbetat utomlands. Det är inte lätt att hitta sitt kall här i livet, 
-                    vad man vill göra när man blir stor. Men nu, äntligen har jag hittat en sysselsättning som är 
-                    spännande, givande och extremt utvecklande. Webbutveckling!
-                </p>
-                <p>
-                    Jag är en pigg, nyfiken och ansvarstagande person som aldrig är rädd att ta mig an nya utmaningar. Har god fysik och är 
-                    van med tunga och kluriga arbetsuppgifter. Jag är envis som en varg och ger sällan eller aldrig upp. Att kunna lära och ta 
-                    in nya saker snabbt och se till att ha huvudet på skaft är egenskaper jag har som är viktigt inom alla arbeten. Dåliga 
-                    egenskaper skulle kunna vara att jag ibland är för noggrann och envis för mitt eget bästa och tiden kan då springa iväg 
-                    på aningen onödiga saker.
-                </p>
-                <p>
-                    Jag studerar just nu Webbutvecklare .NET. Det är en distansutbildning som utgår ifrån Värnamo. Jag söker därför efter 
-                    arbetsgivare belägna i Visby, i första hand, alternativt distansmöjligheter. Mitt mål med utbildningen och framtida 
-                    arbete är att utvecklas så mycket jag bara kan för att öppna upp möjligheterna att exempelvis starta eget bolag.
-                </p>
+            <div className='about'>
+                <div className='about-short'>
+                    <h2>Välkommen!</h2>
+                    <p>
+                        Mitt namn är Johannes och jag utbildar mig till Webbutvecklare i full fart! Jag har en passion för att skapa och 
+                        lära mig nya saker. Här nere kan ni se delar av vad vi har arbetat med hittills i utbildningen.
+                    </p>
+                    <div>
+                        {skills?.map((topic, index) => {
+                            return(
+                                <div key={index} className='topic'>  
+                                    <h4>{topic.title}</h4>
+                                    <div className='skills'>
+                                        {topic.skills?.map((skill, index) => {
+                                            return(
+                                                <div key={index} className='skill'>{skill.title}</div>
+                                            )
+                                        })}
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+
+                <div className='about-more'>
+                    <div className='about-more-title' onClick={() => setMore(!more)}>
+                        <h2>Mer om mig</h2>
+                        <span>{more ? '-' : '+'}</span>
+                    </div>
+                    <div className={more ? 'about-more-content show' : 'about-more-content'}>
+                        <p>
+                            Jag är en 31 årig ung man som är bosatt i Visby. Under min ungdom har jag spenderat många 
+                            sommarlov och helger genom att prova på olika arbetsplatser för att få en bredare blick av arbetslivet. 
+                            Under perioder har jag också varit bosatt och arbetat utomlands. Det är inte lätt att hitta sitt kall här i livet, 
+                            vad man vill göra när man blir stor. Men nu, äntligen har jag hittat en sysselsättning som är 
+                            spännande, givande och extremt utvecklande. Webbutveckling!
+                        </p>
+                        <p>
+                            Jag är en pigg, nyfiken och ansvarstagande person som aldrig är rädd att ta mig an nya utmaningar. Har god fysik och är 
+                            van med tunga och kluriga arbetsuppgifter. Jag är envis som en varg och ger sällan eller aldrig upp. Att kunna lära och ta 
+                            in nya saker snabbt och se till att ha huvudet på skaft är egenskaper jag har som är viktigt inom alla arbeten. Dåliga 
+                            egenskaper skulle kunna vara att jag ibland är för noggrann och envis för mitt eget bästa och tiden kan då springa iväg 
+                            på onödiga småsaker.
+                        </p>
+                        <p>
+                            Jag studerar just nu Webbutvecklare .NET och jag söker aktivt efter min nästa arbetsgivare. Webbutvecklare.NET är en 
+                            distansutbildning som utgår ifrån Värnamo. Jag söker i första hand efter arbetsgivare belägna i Visby, alternativt 
+                            distansmöjligheter. Mitt mål med utbildningen och framtida arbete är att utvecklas så mycket jag bara kan för att i 
+                            framtiden kunna följa min dröm att driva någon form av eget bolag.
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     )
 }
 
 export default Home;
-
-
-
-
-
-/*         <div>
-            <ul className="ul-nav">
-                <li><a href="#hero"><i className="fas fa-home"></i></a></li>
-                <li><a href="#projects" id="project"><i className="fas fa-folder"></i></a></li>
-                <li><a href="#about-link" id="about"><i className="fas fa-user"></i></a></li>
-                <li><a href="#contact-link" id="contact"><i className="fas fa-envelope-open-text"></i></a></li>
-            </ul>
-        </div> */

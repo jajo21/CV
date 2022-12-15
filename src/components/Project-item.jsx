e239e62f898b6f0653449536a98f8f683d746997
Johannes Jakobsson <jajo21pg@student.ju.se>
2022-12-15 11:47:14 +0100

    Ändrade lite i namnen på projekten

--- src/components/Project-item.jsx
+++ src/components/Project-item.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
-function ProjectItem({name, image, id}) {
+function ProjectItem({ name, image, id }) {
   const navigate = useNavigate();
   return (
-    <div 
-        className='project-item' 
-        onClick={() => {navigate('/project/' + id)}}
+    <div
+      className='project-item'
+      onClick={() => { navigate('/project/' + id) }}
     >
-        <div style={{backgroundImage: `url(${image})`}} className="bg-image"></div>
+      <div style={{ backgroundImage: `url(${image})` }} className="bg-image"></div>
+      <div className='project-name'>
         <h2>{name}</h2>
+      </div>
     </div>
   )
 }

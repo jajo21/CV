3c0327486eec3a07851027eafa43bb5e78c0a251
Johannes Jakobsson <jajo21pg@student.ju.se>
2023-04-12 18:22:24 +0200

    deleted images and fixed scripts

--- src/images/images.js
+++ src/images/images.js
@@ -1,16 +1,10 @@
 import * as creperiet from './Jag-creperiet.jpg'
 import * as happyBeach from './Jag-glad-strand.jpg'
-import * as meAndElsa from './Jag-o-elsa.jpg'
-import * as beach from './Jag-strand.jpg'
-import * as me from './Jag.png'
 import * as meRauk from './Jag-rauk.jpg'
 
 const imageUrl = {
     creperiet,
     happyBeach,
-    meAndElsa,
-    beach,
-    me,
     meRauk
 }
 
